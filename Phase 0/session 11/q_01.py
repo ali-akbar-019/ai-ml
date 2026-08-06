9124eee5421569394822cb8f84075e1926b9abd1
Ali Akbar <gamesforever018@gmail.com>
2026-08-05 23:42:02 -0700

    modules

---
+++ Phase 0/session 11/q_01.py
@@ -0,0 +1,14 @@
+import math
+import random
+from datetime import datetime
+print(math.pi)
+print(math.sqrt(144))
+print(math.pow(5, 3))
+print(math.ceil(7.2))
+print(math.floor(7.8))
+print(random.randint(1, 100))
+lst = [1,2,34,5,5,3,2]
+print(lst)
+random.shuffle(lst)
+print(lst)
+print(datetime.now())
